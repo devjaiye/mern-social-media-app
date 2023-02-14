1e6a664d439daaa09d105a08352794d394a32ad4
devjaiye <dayojaiye74@gmail.com>
2023-02-14 01:32:51 +0100

    User Widget and user profile card

--- src/screens/navbar/navbar.jsx
+++ src/screens/navbar/navbar.jsx
@@ -42,11 +42,11 @@ const Navbar = () => {
     const primaryLight = theme.palette.primary.light
     const alt = theme.palette.background.alt
     
-    // const fullName = `${user.firstName} ${user.lastName}`
-    let fullName = '';
-    if (user && user.firstName && user.lastName) {
-         fullName = `${user.firstName} ${user.lastName}`
-        }
+    const fullName = `${user.firstName} ${user.lastName}`
+    // let fullName = '';
+    // if (user && user.firstName && user.lastName) {
+    //      fullName = `${user.firstName} ${user.lastName}`
+    //     }
 
         return (
           <FlexBetween padding="1rem 6%" backgroundColor={alt}>
